c24e6366a6d5e9cf1942d1019083ec1e9911185b
Levy Santiago <levyssantiago@gmail.com>
2024-04-08 15:41:02 -0300

    feat: fetch user checkins history

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,7 +1,9 @@
-import { CheckIn, Prisma } from "@prisma/client";
+import { CheckIn, Prisma } from '@prisma/client'
 
-export interface CheckInsRepository{
+export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,29 +1,35 @@
-import { CheckIn, Prisma } from "@prisma/client";
-import { CheckInsRepository } from "../check-ins-repository";
-import { randomUUID } from "node:crypto";
-import dayjs from "dayjs";
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-  public items: CheckIn[] = [];
+  public items: CheckIn[] = []
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf("date");
-    const endOfTheDay = dayjs(date).endOf("date");
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
 
     const checkInOnSameDate = this.items.find((checkIn) => {
-      const checkInDate = dayjs(checkIn.created_at);
+      const checkInDate = dayjs(checkIn.created_at)
 
       const isOnSameDate =
-        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
 
-      return checkIn.user_id === userId && isOnSameDate;
-    });
+      return checkIn.user_id === userId && isOnSameDate
+    })
 
     if (!checkInOnSameDate) {
-      return null;
+      return null
     }
 
-    return checkInOnSameDate;
+    return checkInOnSameDate
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
@@ -33,10 +39,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
       created_at: new Date(),
-    };
+    }
 
-    this.items.push(checkIn);
+    this.items.push(checkIn)
 
-    return checkIn;
+    return checkIn
   }
 }
