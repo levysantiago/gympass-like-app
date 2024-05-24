7d2116421aa0fcbc218890997aae5b25b7216f2a
Levy Santiago <levyssantiago@gmail.com>
2024-05-23 21:22:11 -0300

    feat: prisma check-ins repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,33 +1,31 @@
-import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
-import { UsersRepository } from "../users-repository";
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
 
-export class PrismaUsersRepository implements UsersRepository{
+export class PrismaUsersRepository implements UsersRepository {
   async findById(userId: string) {
     const userWithSameEmail = await prisma.user.findUnique({
-      where:{
+      where: {
         id: userId,
-      }
+      },
     })
 
     return userWithSameEmail
   }
-  
-  
+
   async findByEmail(email: string) {
     const userWithSameEmail = await prisma.user.findUnique({
-      where:{
+      where: {
         email,
-      }
+      },
     })
 
     return userWithSameEmail
   }
-  
-  
-  async create(data: Prisma.UserCreateInput){
+
+  async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
-      data
+      data,
     })
 
     return user
